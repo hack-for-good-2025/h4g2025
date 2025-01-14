51020e494e446a63006594544175c6d68d7258ad
yxtan02 <yxtan02@gmail.com>
2025-01-14 17:49:54 +0800

    Add signup and login

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,8 +1,10 @@
 const { initializeApp } = require("firebase/app");
+const { getAuth } = require("firebase/auth");
 const { getFirestore } = require("firebase/firestore");
 const firebaseConfig = require("./config.json")
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-module.exports = { db }
+module.exports = { app, db, auth }
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const authRoutes = require('./routes/auth');
 const meetingsRoutes = require('./routes/meetings');
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 
+app.use('/auth', authRoutes);
 app.use('/meetings', meetingsRoutes);
 
 app.listen(port, () => {
